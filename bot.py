910c1d31cac210f006be3b87b0e89e1368bbbca6
metalassdd <101165096+metalassdd@users.noreply.github.com>
2022-03-08 17:05:58 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,8 @@ async def on_ready():
     print('login')
     print(client.user.id)
     print('------------------------')
+    game = discord.Game("/명령어")
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 
 @client.event
@@ -26,13 +28,6 @@ async def on_message(message):
     if message.content != "" and message.channel.id == int(channel):
         with open('chat.txt', 'a', encoding='utf-8') as f:
             f.write(message.author.name + " : " + message.content+"\n\n")
-
-    if message.content.startswith("/채팅"):
-        await message.channel.send(file=discord.File('chat.txt'))
-
-    if message.content.startswith("/삭제"):
-        with open('chat.txt', 'w', encoding='utf-8') as f:
-            f.write("")
 
     if message.content.startswith("/초대"):
         r = message.guild.get_role(int(invite_role))
@@ -80,6 +75,21 @@ async def on_message(message):
                 else:
                     await message.channel.send("이미 인증되어 역할이 부여되었습니다.")
 
+            else:
+                await message.channel.send("멤버 명단에 없습니다.")
+
+################################################################################################
+
+    if not message.author.guild_permissions.administrator:
+        return
+
+    if message.content.startswith("/채팅"):
+        await message.channel.send(file=discord.File('chat.txt'))
+
+    if message.content.startswith("/삭제"):
+        with open('chat.txt', 'w', encoding='utf-8') as f:
+            f.write("")
+
     if message.content.startswith("/멤버추가"):
         with open('member.txt', 'a', encoding='utf-8') as file:
             file.write(message.content[6:]+"\n")
@@ -94,4 +104,15 @@ async def on_message(message):
     if message.content.startswith("/멤버확인"):
         await message.channel.send(file=discord.File('member.txt'))
 
-client.run("OTUwMjM1ODM5NjU5NjUxMTg1.YiV-Fg.GKI4cqezdCm8VqMeZMypCBAKcZs")
+    if message.content.startswith("/명령어"):
+        await message.channel.send("```/채팅 - 채팅백업파일 다운\n"
+                                   "/삭제 - 백업파일 리셋\n"
+                                   "/멤버추가 - 인증멤버 추가(/멤버추가 유저이름)\n"
+                                   "/멤버삭제 - 인증멤버 삭제(/멤버삭제 유저이름)\n"
+                                   "/멤버확인 - 인증멤버 목록확인\n"
+                                   "/초대 - 본인의 초대자 수 확인\n"
+                                   "/순위 - 초대 순위\n"
+                                   "/인증 - 명단에 본인 이름이 있으면 역할획득```")
+
+token = os.environ["TOKEN"]
+client.run(token)
